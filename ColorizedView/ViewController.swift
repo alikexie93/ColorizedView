41f4846f1a7a548ad7ecaa8ae77d115e207c3f71
aleksei <alikexie93@yandex.ru>
2023-03-26 23:32:56 +0600

    Write color changing logic

--- ColorizedView/ViewController.swift
+++ ColorizedView/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //MARK: - IBOutlets
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+    @IBOutlet var colorizedView: UIView!
+
+    //MARK: - IBActions
+    @IBAction func sliderChangeColor() {
+        redSliderValue.text = redSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
+        greenSliderValue.text = greenSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
+        blueSliderValue.text = blueSlider.value.formatted(
+            .number.precision(.fractionLength(2))
+        )
+        
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
-
-
+    
 }
 
